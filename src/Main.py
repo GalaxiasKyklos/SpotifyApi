80dab261aa5dc95396948d1dd0375fccaf715715
GalaxiasKyklos <saul_enrique-7285-96@hotmail.com>
2022-04-24 13:32:23 -0700

    Update main

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,10 @@
+# Dotenv config
 from dotenv import load_dotenv
-
 load_dotenv()  # take environment variables from .env.
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+print('Running from main!')
 
 
